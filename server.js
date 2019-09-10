eefd8cef05a22e565a01c0e0e01db43698695bd8
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-10 17:43:11 +0530

    crud

--- server.js
+++ server.js
@@ -25,6 +25,24 @@ app.post("/customer", (req, res) => {
     })
     .catch(err => console.log(err));
 });
+app.get("/customer/:id", (req, res) => {
+    Customer.findOne({ _id: req.params.id })
+  
+      .then(result => res.status(200).send(result))
+      .catch(err => res.status(400).send(err))
+  })
+  app.patch("/customer/:id", (req, res) => {
+    Customer.findOneAndUpdate({ _id: req.params.id }, req.body)
+  
+      .then(result => res.status(200).send(result))
+      .catch(err => res.status(400).send(err))
+  })
+  app.delete("/customer/:id", (req, res) => {
+    Customer.findOneAndDelete({ _id: req.params.id })
+  
+      .then(result => res.status(200).send(result))
+      .catch(err => res.status(400).send(err))
+  })
 
 app.listen(8080, () => console.log("server is running on port 8080"));
 
