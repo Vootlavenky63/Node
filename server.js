b7e7aaa8317dca02c62ec05c61efb98437897d8d
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-10 15:05:38 +0530

    FirstRes from server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const express=require("express")
 const app=express()
+app.get('/',(req,res)=>{
+    res.send("hi from express server back end")
+})
 app.listen(8080,()=>console.log("server is running on port 8080"))
