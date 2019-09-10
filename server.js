e49fb3c4a034aea4fb03c3e65b919904111e4b49
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-10 17:00:49 +0530

    firstpost method

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express=require("express")
-const app=express()
-var db=require('./db/mongoose')
-var Customer=require('./models/Customer')
+const express = require("express");
+const bodyParser=require("body-parser")
+const app = express();
+var db = require("./db/mongoose");
+var Customer = require("./models/Customer");
 app.get("/customer", (req, res) => {
-    Customer.find({})
-      .then(result => {
-        return Customer.find({})
-      })
-      .then(result => res.status(200).send(result))
-      .catch(err => res.status(400).send(err))
-  })
+  Customer.find({})
+    .then(result => {
+      return Customer.find({});
+    })
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(400).send(err));
+});
+// app.post('/customer',(req,res)=>{
+//     res.send("successfully posted")
+// })
+app.use(bodyParser.json());
+app.post("/customer", (req, res) => {
+  var customer = new Customer(req.body);
+  customer
+    .save()
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch(err => console.log(err));
+});
 
-
-  
-    
-
-app.listen(8080,()=>console.log("server is running on port 8080"))
-
-
-
-
+app.listen(8080, () => console.log("server is running on port 8080"));
 
 // mongodb+srv://venkatesh:venkatesh@cluster0-bilop.mongodb.net/test?retryWrites=true&w=majority
