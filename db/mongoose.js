b154d9e410d7530d8e43949221c5aa3562174c41
vootlavenky63 <vootla.venky63@gmail.com>
2019-09-10 15:50:41 +0530

    connecting to mongoose

---
+++ db/mongoose.js
@@ -0,0 +1,4 @@
+const mongoose=require("mongoose")
+mongoose.Promise=global.Promise
+var db=mongoose.connect("mongodb+srv://venkatesh:venkatesh@cluster0-bilop.mongodb.net/test?retryWrites=true&w=majority")
+module.exports=db
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express=require("express")
 const app=express()
 app.get('/',(req,res)=>{
-    res.send("hi from express server back end")
+    res.send({
+        name:"venky",
+        age:25,
+        role:"fullStack Developer",
+        developer:true
+
+    })
 })
 app.listen(8080,()=>console.log("server is running on port 8080"))
+
+
+
+
+
+// mongodb+srv://venkatesh:venkatesh@cluster0-bilop.mongodb.net/test?retryWrites=true&w=majority
